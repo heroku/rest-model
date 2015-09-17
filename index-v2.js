6e6e4acaaa13061702eb86c82c4fb8f2986e159e
Josh W Lewis <josh.w.lewis@gmail.com>
2015-09-17 12:49:06 -0500

    Don't try to set the primaryKey to a function when fetching.

--- index-v2.js
+++ index-v2.js
@@ -955,7 +955,7 @@ module.exports = Ember.Object.extend({
    */
   getUpdatableProperties: function(model) {
     var keys = Ember.keys(model).filter(function(key) {
-      return ['originalProperties', 'dirtyProperties'].indexOf(key) === -1;
+      return ['primaryKey', 'originalProperties', 'dirtyProperties'].indexOf(key) === -1;
     });
 
     return model.getProperties(keys);
